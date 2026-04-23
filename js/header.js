82980154184a3f4b410538c65fd6edf601a4c896
emily-ejag <emily.artegar@gmail.com>
2026-04-23 16:28:25 -0700

    Updates

--- js/header.js
+++ js/header.js
@@ -1,28 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
+document.addEventListener('DOMContentLoaded', function() {
+  const headerHTML = `
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8L4B7J2TSN"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8L4B7J2TSN"><\/script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-
       gtag('config', 'G-8L4B7J2TSN');
-    </script>
+    <\/script>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fuggles&family=Quicksand&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style2.css">
+  `;
 
-    <title>Bryam Astudillo</title>
-    
-  </head>
-  
-  <body class="my-body">
+  const navbarHTML = `
     <div class="navbar">
       <div id="brand">Bryam Astudillo</div>
       <div>
@@ -33,7 +27,21 @@
         <a href="tutorial.html" >Knowledge Hub</a>
       </div>
     </div>
-    <div id="root"></div>
-    <iframe class="tutorial-frame" src="https://bastudil94.github.io/tutorials/docs/initialsetup/"></iframe>
-  </body>
-</html>
+  `;
+
+  const headElement = document.querySelector('head');
+  if (headElement) {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = headerHTML;
+    while (tempDiv.firstChild) {
+      headElement.appendChild(tempDiv.firstChild);
+    }
+  }
+
+  const navContainer = document.querySelector('body');
+  if (navContainer) {
+    const navDiv = document.createElement('div');
+    navDiv.innerHTML = navbarHTML;
+    navContainer.insertBefore(navDiv.firstChild, navContainer.firstChild);
+  }
+});
